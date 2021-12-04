61091d30f32da8d1629e4738c3ae2b1a8b203727
Catink123 <qwerty.qaz120@gmail.com>
2021-12-04 23:29:36 +0300

    environment file explanation

--- src/environments/environment_example.ts
+++ src/environments/environment_example.ts
@@ -1,4 +1,10 @@
+/* 
+    This file shows an example of an environment.ts file. 
+    In the regular environment.ts you may put your developer key,
+    and in environment.prod.ts you may put your production key to use on a public website.
+ */
+
 export const environment = {
-    production: false,
+    production: false, // true on environment.prod.ts
     riotAPIKey: 'your-api-key' // insert your API Key from the Developer Portal
 }
